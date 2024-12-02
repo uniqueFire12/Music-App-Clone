27aa6d2b863f23ddd772bd9c36537135e18fc85c
Ananya Ganguly <ananya.ganguly1204@gmail.com>
2024-12-02 12:20:58 +0530

    New commit

---
+++ spotify-clone/src/App.jsx
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import Display from "./Components/Display";
+import Player from "./Components/Player";
+import Sidebar from "./Components/Sidebar";
+import { PlayerContext } from "./Context/PlayerContext";
+
+function App() {
+  const { audioRef, track } = useContext(PlayerContext);
+
+  return (
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex">
+        <Sidebar />
+        <Display /> 
+      </div>
+      <Player/>
+      <audio ref={audioRef} src={track.file} preload="auto"></audio>
+    </div>
+  )
+}
+
+
+export default App
